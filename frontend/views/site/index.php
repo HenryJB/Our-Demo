6c54f369bf5dfaab6c5eb11198c4821046279461
iameronmosele <crayolu@gmail.com>
2018-04-02 08:09:12 +0100

    editing adding to cart page

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -4,6 +4,7 @@
 /* @var $content string */
 
 use yii\helpers\Html;
+use common\models\Product;
 
 ?>
 
@@ -86,70 +87,9 @@ use yii\helpers\Html;
     </div>
   <!--banner-bottom-->
   <!--new-arrivals-->
-    <div class="new-arrivals-w3agile">
-      <div class="container">
-        <h2 class="tittle">New Arrivals</h2>
-        <div class="arrivals-grids">
-          <?php foreach($products as $product): ?>
-            <div class="col-md-3 arrival-grid simpleCart_shelfItem">
-            <div class="grid-arr">
-              <div  class="grid-arrival">
-                <figure>
-                  <a href="#" class="new-gri" data-toggle="modal" data-target="#myModal1">
-                    <div class="grid-img">
-                        <?php echo Html::img('@web/images/'.$product->imageUrl, $options=['class'=> 'img-responsive']) ?>
-                    </div>
-                    <div class="grid-img">
-                        <?php echo Html::img('@web/images/'.$product->imageUrl2, $options=['class'=> 'img-responsive']) ?>
-                    </div>
-                  </a>
-                </figure>
-              </div>
-              <div class="ribben">
-                <p>NEW</p>
-              </div>
-              <div class="ribben1">
-                <p>SALE</p>
-              </div>
-              <div class="block">
-                <div class="starbox small ghosting"> </div>
-              </div>
-              <div class="women">
-                <h6><a href="single.html">Sed ut perspiciatis unde</a></h6>
-                <span class="size">XL / XXL / S </span>
-                <p ><del>$100.00</del><em class="item_price">$70.00</em></p>
-                
-                <button type="button" class="my-cart-b item_add" data-toggle="modal" 
-                data-target='#myModal<?= $product->id; ?>'>
-                  Add To Cart
-                </button>
-
-                <!-- Modal -->
-                <div class="modal fade" id="myModal<?= $product->id; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-                <div class="modal-dialog" role="document">
-                  <div class="modal-content">
-                    <div class="modal-header">
-                      <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                      <h4 class="modal-title" id="myModalLabel"><?= $product->name; ?></h4>
-                    </div>
-                    <div class="modal-body">
-                      
-                    </div>
-                    <div class="modal-footer">
-                      <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                      <button type="button" class="btn btn-primary">Save changes</button>
-                    </div>
-                  </div>
-                </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <?php endforeach; ?>
-          <div class="clearfix"></div>
-        </div>
-      </div>
-    </div>
+    <?=$this->render('_productpartial.php', [
+        'products' => $products
+    ]);?>
 
     <!-- Button trigger modal -->
 
